c4be1b5eba9dc1eb1811b7327a79c60339424edb
Hassan Uriostegui <hassan@spree3d.com>
2022-08-29 00:21:13 -0700

    passing tests OK

--- NavigatorUITests/NavigatorUITests.swift
+++ NavigatorUITests/NavigatorUITests.swift
@@ -10,7 +10,10 @@ import XCTest
 
 
 class NavigatorUITests: XCTestCase {
+    let app = XCUIApplication()
+   
     override func setUpWithError() throws {
+        app.launch()
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
         // In UI tests it is usually best to stop immediately when a failure occurs.
@@ -20,14 +23,14 @@ class NavigatorUITests: XCTestCase {
     }
 
     override func tearDownWithError() throws {
+//        app.terminate()
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
     func testTabs() throws {
         
         
-        let app = XCUIApplication()
-        app.launch()
+       
         app/*@START_MENU_TOKEN@*/.buttons["Tab Home"]/*[[".buttons[\"Home\"]",".buttons[\"Tab Home\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tapSlow()
         let tabStoreButton = app/*@START_MENU_TOKEN@*/.buttons["Tab Store"]/*[[".buttons[\"Store\"]",".buttons[\"Tab Store\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
         tabStoreButton.tapSlow()
