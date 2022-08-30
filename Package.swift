1b7defccdf12f7ca30df9d755807826ecdc08075
Hassan Uriostegui <hassan@spree3d.com>
2022-08-29 17:01:35 -0700

    wip

--- Package.swift
+++ Package.swift
@@ -20,9 +20,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "NavigatorLib",
-            dependencies: []),
-        .testTarget(
-            name: "NavigatorLibTests",
-            dependencies: ["NavigatorLib"]),
+            path: "Navigator"
+        ),
+        
     ]
 )
