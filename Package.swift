36003763f0b797c5e99f83be6d343dc7d0c79bb9
Hassan Uriostegui <hassan@spree3d.com>
2022-12-15 20:38:36 -0800

    fix

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/spree3d/ios-coreui", exact: "1.0.10")
+        .package(url: "https://github.com/spree3d/ios-coreui", exact: "1.0.12")
     ],
     targets: [
         .target(
