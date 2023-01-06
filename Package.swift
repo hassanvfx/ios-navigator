266ac0f913c36584d42799ce5f63e25f85d7dc61
Hassan Uriostegui <hassan@spree3d.com>
2023-01-05 19:10:43 -0800

    choretag

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/spree3d/ios-coreui", exact: "3.0.0")
+        .package(url: "https://github.com/spree3d/ios-coreui", exact: "3.0.1")
     ],
     targets: [
         .target(
