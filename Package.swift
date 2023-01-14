0965f6e67be2aee96e5af22ea57751a02676adfd
Hassan Uriostegui <hassan@spree3d.com>
2023-01-14 00:44:56 -0800

    Bump version to 3.0.7

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/spree3d/ios-coreui", exact: "3.0.8")
+        .package(url: "https://github.com/spree3d/ios-coreui", exact: "3.0.9")
     ],
     targets: [
         .target(
